f7b2331058d2b3eb3e89299daeee78aecb9980b5
unknown <alexlotte16@gmail.com>
2018-10-26 00:46:38 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+// TODO: do this with a mutation observer instead
 
+const onmountEvent = new Event('onmount')
+const onmodifyEvent = new Event('onmodify')
+const ondeleteEvent = new Event('ondelete')
 
+const realCreateElement = document.createElement.bind(document)
+
+document.createElement = (name) => {
+  let el = realCreateElement(name)
+  el.addEventListener("DOMNodeInserted", ev => el.dispatchEvent(onmountEvent), false);
+  el.addEventListener("DOMAttrModified", ev => el.dispatchEvent(onmodifyEvent), false);
+  el.addEventListener("DOMNodeRemoved", ev => el.dispatchEvent(ondeleteEvent), false);
+  return el
+}
