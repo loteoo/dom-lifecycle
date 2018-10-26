9d39a9cd71a03d26981429c390d8ba444d3b48a4
unknown <alexlotte16@gmail.com>
2018-10-26 03:01:01 -0400

    this might be ok

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 
-
-const css = `
-  @keyframes nodeInserted {  
-    from {opacity: 0.99;}
-    to {opacity: 1;}
-  }
-  * {
-    animation-duration: 0.001s;
-    animation-name: nodeInserted;
-  }
-`
-
-const head = document.head || document.getElementsByTagName('head')[0]
-const style = document.createElement('style')
-style.type = 'text/css';
-style.appendChild(document.createTextNode(css));
-head.appendChild(style);
-
-
-
 const mountEvent = new Event('mount')
-const modifyEvent = new Event('modify')
-const deleteEvent = new Event('delete')
 
 const realCreateElement = document.createElement.bind(document)
-
-
-
 document.createElement = (name) => {
   let el = realCreateElement(name)
-  el.addEventListener("animationstart", ev => el.dispatchEvent(mountEvent), false);
-  el.addEventListener("MSAnimationStart", ev => el.dispatchEvent(mountEvent), false);
-  el.addEventListener("webkitAnimationStart", ev => el.dispatchEvent(mountEvent), false);
+  setTimeout(() => el.dispatchEvent(mountEvent))
   return el
 }
