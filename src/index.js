d6cd85d99d441b7aaf906527cca84fb8358f2603
unknown <alexlotte16@gmail.com>
2018-10-26 01:53:34 -0400

    you will need a shower after this

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-// TODO: do this with a mutation observer instead
 
-const onmountEvent = new Event('onmount')
-const onmodifyEvent = new Event('onmodify')
-const ondeleteEvent = new Event('ondelete')
+
+const css = `
+  @keyframes nodeInserted {  
+    from {opacity: 0.99;}
+    to {opacity: 1;}
+  }
+  * {
+    animation-duration: 0.001s;
+    animation-name: nodeInserted;
+  }
+`
+
+const head = document.head || document.getElementsByTagName('head')[0]
+const style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(css));
+head.appendChild(style);
+
+
+
+const mountEvent = new Event('mount')
+const modifyEvent = new Event('modify')
+const deleteEvent = new Event('delete')
 
 const realCreateElement = document.createElement.bind(document)
 
+
+
 document.createElement = (name) => {
   let el = realCreateElement(name)
-  el.addEventListener("DOMNodeInserted", ev => el.dispatchEvent(onmountEvent), false);
-  el.addEventListener("DOMAttrModified", ev => el.dispatchEvent(onmodifyEvent), false);
-  el.addEventListener("DOMNodeRemoved", ev => el.dispatchEvent(ondeleteEvent), false);
+  el.addEventListener("animationstart", ev => el.dispatchEvent(mountEvent), false);
+  el.addEventListener("MSAnimationStart", ev => el.dispatchEvent(mountEvent), false);
+  el.addEventListener("webkitAnimationStart", ev => el.dispatchEvent(mountEvent), false);
   return el
 }
